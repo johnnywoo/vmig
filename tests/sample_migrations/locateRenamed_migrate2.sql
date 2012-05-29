4727195006da0c039856ab66137f7e6ee092d2c9
agalkin <agalkin@ocg.ru>
2012-05-29 15:45:25 +0400

    Fixed tests for migration generator (SET NAMES was not accounted for)

--- tests/sample_migrations/locateRenamed_migrate2.sql
+++ tests/sample_migrations/locateRenamed_migrate2.sql
@@ -4,20 +4,28 @@
 
 -- down: 0004_create_304.sql
 
+SET NAMES cp1251;
+
 ALTER TABLE `vmig_test`.`test1`
   DROP COLUMN `field304`;
 
 -- down: 0003_create_303.sql
+
+SET NAMES cp1251;
 
 ALTER TABLE `vmig_test`.`test1`
   DROP COLUMN `field303`;
 
 -- up: 0003_create_303.sql
 
+SET NAMES cp1251;
+
 ALTER TABLE `vmig_test`.`test1`
   ADD COLUMN `field303` int(1) NOT NULL AFTER `field302`;
 
 -- up: 0004_create_304.sql
+
+SET NAMES cp1251;
 
 ALTER TABLE `vmig_test`.`test1`
   ADD COLUMN `field304` int(1) NOT NULL AFTER `field303`;
--- tests/sample_migrations/locateRenamed_migrate3.sql
+++ tests/sample_migrations/locateRenamed_migrate3.sql
@@ -1,40 +1,56 @@
 
 -- down: 0004_create_304.sql
 
+SET NAMES cp1251;
+
 ALTER TABLE `vmig_test`.`test1`
   DROP COLUMN `field304`;
 
 -- down: 0003_create_303.sql
+
+SET NAMES cp1251;
 
 ALTER TABLE `vmig_test`.`test1`
   DROP COLUMN `field303`;
 
 -- down: 0002_create_302_renamed2.sql
 
+SET NAMES cp1251;
+
 ALTER TABLE `vmig_test`.`test1`
   DROP COLUMN `field302`;
 
 -- down: 0001_create_301.sql
+
+SET NAMES cp1251;
 
 ALTER TABLE `vmig_test`.`test1`
   DROP COLUMN `field301`;
 
 -- up: 0001_create_301.sql
 
+SET NAMES cp1251;
+
 ALTER TABLE `vmig_test`.`test1`
   ADD COLUMN `field301` int(1) NOT NULL AFTER `field5`;
 
 -- up: 0002_create_302_renamed3.sql
+
+SET NAMES cp1251;
 
 ALTER TABLE `vmig_test`.`test1`
   ADD COLUMN `field302` int(1) NOT NULL AFTER `field301`;
 
 -- up: 0003_create_303.sql
 
+SET NAMES cp1251;
+
 ALTER TABLE `vmig_test`.`test1`
   ADD COLUMN `field303` int(1) NOT NULL AFTER `field302`;
 
 -- up: 0004_create_304.sql
+
+SET NAMES cp1251;
 
 ALTER TABLE `vmig_test`.`test1`
   ADD COLUMN `field304` int(1) NOT NULL AFTER `field303`;
